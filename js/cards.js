f631884af79a24a9bfc5087ba8b739205b0a0e93
a-wotton <a_wotton@fanshaweonline.ca>
2025-11-13 15:19:28 -0500

    implemented more game features

--- js/cards.js
+++ js/cards.js
@@ -1,5 +1,5 @@
-const suits = ['diamonds', 'clubs', 'hearts', 'spades']
-const values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+const suits = ['diamonds', 'clubs', 'hearts', 'spades'];
+const values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
 
 const deck = []
 
